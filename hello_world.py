ad5db8cce7029d9cd1c643804324dd7b61d090a8
suix0 <bagasbas.nethangabriel@gmail.com>
2023-01-13 03:23:41 +0800

    added imissher cuz i miss her

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,14 @@
 for i in range(10):
     print(i)
 
-for in range(5):
+for i in range(5):
     print ("stop raining yo")
+
+# I miss her
+imissher = True
+while imissher == True:
+    print("I miss her")
+    imissher = False
 
 # anohter change in the fork
 # hwnlo man iam suixzeros
